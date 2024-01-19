15ecd0e286381e90e0de63925af0ad3c1ada94ed
SSH <songlink@SSHui-MacBookPro.local>
2024-01-19 16:57:53 +0900

    스킬 추가

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,16 +1,15 @@
 import aboutImg from "../assets/image/about.jpeg";
+const introText = {
+  title: "Song Gandhi portfolio",
+  desc: ["제발", "공부하자", "안하고 후회하지 말자"]
+};
 
 function Intro() {
-  const introText = {
-    title: "Song Gandhi portfolio",
-    desc: ["제발", "공부하자", "안하고 후회하지 말자"]
-  };
-
   return (
     <section id="intro">
-      <div className="intro_inner">
-        <h1 className="intro_title">{introText.title}</h1>
-        <div className="intro_lines" ariaHidden="true">
+      <div className="intro__inner">
+        <h1 className="intro__title">{introText.title}</h1>
+        <div className="intro__lines" ariaHidden="true">
           <span className="line"></span>
           <span className="line"></span>
           <span className="line"></span>
@@ -25,7 +24,7 @@ function Intro() {
             <div>{introText.desc[1]}</div>
             <div>{introText.desc[2]}</div>
           </div>
-          <div className="img"><img src={aboutImg} alt="어바웃"/></div>
+          <div className="img"><img src={aboutImg} alt="어바웃" /></div>
         </div>
         <div className="intro__lines bottom" ariaHidden="true">
           <span className="line"></span>
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,5 +1,35 @@
+const skillText = [
+  {
+    title: "React",
+    desc: "리엑트를 이용하여 어느정도 까지 해봤고 어느 기술까지 써봤다"
+  },
+  {
+    title: "Angular",
+    desc: "Angular 1버전"
+  },
+  {
+    title: "SQL (MSSQL, MySQL)",
+    desc: "MSSQL 쿼리를 이용"
+  }
+]
+
 function Skill() {
-  return(<div>Skill</div>);
+  return (
+    <section id="skill">
+      <div className="skill__inner">
+        <h2 className="skill__title">나의 기술 <em> 스킬 </em></h2>
+        <div className="skill__desc">
+          {skillText.map((skill, key) => (
+            <div key={key}>
+              <span>{key + 1}.</span>
+              <h3>{skill.title}</h3>
+              <p>{skill.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Skill;
